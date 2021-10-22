c4c6be53a98d054474a6cae9616300a1d38a65cd
Andrea Bellesia <88466180+ABellesia@users.noreply.github.com>
2021-10-22 17:11:15 +0000

    Bien hecho

--- Practica2/main.py
+++ Practica2/main.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 19:36:17 2021
+
+@author: Diego Arellano
+"""
 
 #Definimos la clase nodo
 class Nodo:
     def __init__(self, dato=None):
         self.dato = dato
         self.hijo_der = None
-        self.datohijo_izq = None
+        self.hijo_izq = None
         self.papa = None
 
     def set_hijo_der(self, dato):
@@ -13,19 +19,19 @@ class Nodo:
     def set_hijo_izq(self, dato):
         self.hijo_izq = Nodo(dato)
 
-    def get_dato():
+    def get_dato(self):
         return self.dato
 
-    def get_papa():
+    def get_papa(self):
         return self.papa
 
     def set_papa(self, papa):
         self.papa = papa
 
-    def get_hijo_der():
+    def get_hijo_der(self):
         return self.hijo_der
 
-    def get_hijo_izq():
+    def get_hijo_izq(self):
         return self.hijo_izq
 
 class Arbol_binario:
@@ -35,13 +41,20 @@ class Arbol_binario:
           self.cont = 0
       else: 
           self.cont = 1
-  def size():
+  def size(self):
       return self.cont
-  def postOrden(nodo):
-    if nodo:
-      postOrden(nodo.getIzq())
-      postOrden(nodo.getDer())
-      print(nodo.getDato())
+  
+  def get_raiz(self):
+      return self.raiz  
+
+  def post_ordenR(self):
+      self.post_orden(self.get_raiz())
+      
+  def post_orden(self, nodo):
+      if nodo != None:
+          self.post_orden(nodo.hijo_izq)
+          self.post_orden(nodo.hijo_der)
+          print(nodo.get_dato())
 
     
 
@@ -53,133 +66,83 @@ class Arbol_de_expresion(Arbol_binario):
             self.cont = 1
         else:
             self.cont = 0
-        self.arbol_izq = arbol_izq
-        self.arbol_der = arbol_der
+        self.raiz.set_hijo_izq(arbol_izq)
+        self.raiz.set_hijo_der(arbol_der)
         if arbol_der != None:
-            self.count = self.count + arbol_der.size()
+            self.cont = self.cont + arbol_der.size()
             self.raiz.set_hijo_der(arbol_der.get_raiz())
         if arbol_izq != None:
-            self.count = self.count + arbol_izq.size()
+            self.cont = self.cont + arbol_izq.size()
             self.raiz.set_hijo_izq(arbol_izq.get_raiz())
 
-    def get_raiz():
-        return self.raiz
-
-    def evalua_arbol():
-        return evalua_nodo(self.raiz)
-        
-    def evalua_nodo(self, raiz):
-        if raiz == None: 
-            res = 0
-        else: 
-            temp = raiz.get_dato()
-            if es_Operador(temp) == True: 
-                operando_1 = raiz.get_izq()
-                operando_2 = raiz.get_der()
-                res = calcula_termino(temp,operando_1, operando_2) 
-            else:
-                res = temp.get_Dato()
-        return res
-
-  
-
-  def calcula_termino(operador, operando_1, operando_2):
-    res=0
-    if operador == '+':
-      res = operando_1 + operando_2
-      elif operador == '-':
-        res = operando_1 - operando_2
-      elif operador == '*':
-        res = operando_1 * operando_2
-        else
-          res = operando_1 / operando_2
-        return res
-        
-        
 
 
-  def es_Operador(dato):
-      operadores = ['+','-','*','/']
-      if dato in operadores:
-          res = True
-      else:
-          res = False
-      return res
 
-
-class Pila(): 
+class Pila: 
     def __init__(self):
         self.pila = []
         
-    def push(dato):
+    def push(self, dato):
         self.pila.append(dato)
         
-    def pop():
-        self.pila.pop()
+    def pop(self):
+        return self.pila.pop()
         
-    def is_Empty(): 
+    def is_empty(self): 
         return len(self.pila) == 0
         
-    def peek():
+    def peek(self):
         return self.pila[-1]
         
 class Calculadora:
-  def __init__(self, expresion):
-    self.expresion = expresion
-
-  def post_fija(expresion):
-      return expresion_post_fija
-
-  def es_Operador(dato):
-      operadores = ['+','-','*','/']
-      if dato in operadores:
-          res = True
-      else:
-          res = False
-      return res
-
-  def es_Operando(dato): 
-      try:
-          float(dato)
-          return True
-      except ValueError:
-          return False
-  
-  def jerarquia(signo):
-    if(signo == "+" or signo == "-"):
-      return 1
-    elif(signo == "*" or signo == "/"):
-      return 2
-    return 0
-  
-  def crear_arbol(pila_a, pila_b):
-    der = pila_b.pop()
-    izq = pila_b.pop()
-    pila_b.push(Arbol_de_expresion(pila_a.pop(), izq, der))
-
-
-  def evaluar_expresion(expresion): 
-    pila_a = Pila()
-    pila_b = Pila()
-    tokens = self.expresion.split()
-    while len(tokens) != 0:
-      token = tokens.pop(0)
-      if es_Operando(token): 
-        pila_b.push(Arbol_de_expresion(float(token)))
-      elif token == "(":
-        pila_a.push(token)
-      elif token == ")":
-        while(pila_a.peek() != "("):
-          crear_arbol(pila_a, pila_b)
-        pila_a.pop()
-      else: 
-        while(not pila_a.isEmpty() and pila_a.peek() != "(" and jerarquia(pila_a.peek()) >= jerarquia(token)):
-          crear_arbol(pila_a, pila_b)
-
-    while(not pila_a.isEmpty()):
-      crear_arbol(pila_a, pila_b)
     
-    return pila_b.pop()
+    def es_operando(self, dato): 
+        try:
+            float(dato)
+            return True
+        except ValueError:
+            return False
+  
+    def jerarquia(self, signo):
+      if signo == "+" or signo == "-" :
+          return 1
+      elif signo == "*" or signo == "/":
+          return 2
+      return 0
+  
+    def crear_arbol(self, pila_a, pila_b):
+        der = pila_b.pop()
+        izq = pila_b.pop()
+        pila_b.push(Arbol_de_expresion(pila_a.pop(), izq, der))
 
-arbolito = evaluar_expresion("( 2 - 6 ) * 3 + 5")
-arbolito.postOrden()   
+
+    def evaluar_expresion(self, expresion): 
+        pila_a = Pila()
+        pila_b = Pila()
+        tokens = expresion.split()
+        while len(tokens) > 0:
+            token = tokens.pop(0)
+            if self.es_operando(token):
+                pila_b.push(Arbol_de_expresion(float(token)))
+            elif token == "(" :
+                pila_a.push(token)
+            elif token == ")" :
+                while(pila_a.peek() != "("):
+                    self.crear_arbol(pila_a, pila_b)
+                pila_a.pop()
+            else: 
+                while(not pila_a.is_empty() and pila_a.peek() != "(" and self.jerarquia(pila_a.peek()) >= self.jerarquia(token)):
+                    self.crear_arbol(pila_a, pila_b)
+                pila_a.push(token)
+                    
+        while(not pila_a.is_empty()):
+            self.crear_arbol(pila_a, pila_b)
+
+        arbol = pila_b.pop()
+        arbol.post_ordenR()
+
+
+calcu = Calculadora()
+calcu.evaluar_expresion(" 2 * 6  / 3 * 5")
+
+
